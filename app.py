6d9fb9ed7d891f8a0cfe102f658b13c0f1d23acf
pcheung5594 <ppc5025@gmail.com>
2018-04-20 18:48:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,24 +55,17 @@ def processRequest(req):
         return {}
     baseurl = "http://ec2-18-217-98-95.us-east-2.compute.amazonaws.com:8080/CloudComputing/article"
     context = ssl._create_unverified_context()
-    #result = req.get("result")
-    #yql_query = makeYqlQuery(req)
-    #if yql_query is None:
-    #    return {}
-    #yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    #yql_url = requests.post(baseurl, json = req.get("result"))
-    #####yql_url = requests.post(baseurl, json = {'id': '76314cf2-1a90-4388-a7f8-f7f786a10827', 'timestamp': '2018-04-17T09:29:01.184Z', 'lang': 'en', 'result': {'source': 'agent', 'resolvedQuery': 'Cloud Computing', 'action': 'DBLPSearch', 'actionIncomplete': False, 'parameters': {'author': ['CS John'], 'title': ['Cloud Computing'], 'year': ['2011'], 'id': ''}, 'contexts': [], 'metadata': {'intentId': '7f288f89-818a-495e-ae25-1ac40e63d564', 'webhookUsed': 'true', 'webhookForSlotFillingUsed': 'false', 'webhookResponseTime': 5035, 'intentName': 'look_for_article'}, 'fulfillment': {'speech': 'We would like to suggest you to read "Cocoa: Dynamic Container-Based Group Buying Strategies for Cloud Computing" from this John C. S. Lui and it is published in 2011.', 'messages': [{'type': 0, 'speech': 'We would like to suggest you to read "Cocoa: Dynamic Container-Based Group Buying Strategies for Cloud Computing" from this  John C. S. Lui and it is published in 2011.'}]}, 'score': 1}, 'status': {'code': 206, 'errorType': 'partial_content', 'errorDetails': 'Webhook call failed. Error: Webhook response was empty.', 'webhookTimedOut': False}, 'sessionId': '67a43021-aac2-4352-a878-ecbf4f612045'})
-    #yql_url = requests.post(baseurl,data = {'key':'value'})
+    
     #data2 = b'{"id": "d33afbce-baec-4194-b677-55e3a62ffd88", "timestamp": "2018-03-31T12:03:52.095Z", "lang": "en", "result": {"source": "agent", "resolvedQuery": "Cloud Computing", "action": "", "actionIncomplete": False, "parameters": {"authors": ["Anna M. Bianchi"], "title": ["Role of signal"], "year": ["2007"], "id": ""}, "contexts": [], "metadata": {"intentId": "7f288f89-818a-495e-ae25-1ac40e63d564", "webhookUsed": "true", "webhookForSlotFillingUsed": "false", "intentName": "look_for_article"}, "fulfillment": {"speech": "", "messages": [{"type": 0, "speech": ""}]}, "score": 1}, "status": {"code": 200, "errorType": "success", "webhookTimedOut": False}, "sessionId": "4e513bc9-4744-4f63-87d8-68f08f2f33c8"}'
-    data2 = json.dumps(req).replace("\",'')[1:-1].encode()
-    yql_url = Request(baseurl, data2, headers={'User-agent': 'Mozilla 5.10', 'Content-type': 'application/json', 'Accept': 'application/json'})
-    try:
-        handler = urlopen(yql_url)
-    except HTTPError as e:
-        print(e)
-    datax = json.loads(handler.read().decode().replace("\\",'')[1:-1])
+    data2 = json.dumps(req).replace("\",'').encode()
+    #yql_url = Request(baseurl, data2, headers={'User-agent': 'Mozilla 5.10', 'Content-type': 'application/json', 'Accept': 'application/json'})
+    #try:
+    #    handler = urlopen(yql_url)
+    #except HTTPError as e:
+    #    print(e)
+    #datax = json.loads(handler.read().decode().replace("\\",'')[1:-1])
     #data = "test"
-    res = makeWebhookResult(datax)
+    res = makeWebhookResult(data2)
     #return res
     return res
 
@@ -124,7 +117,7 @@ def makeWebhookResult(data):
         "speech": "test",
         #"speech": data['displayText'],
         #"displayText": data['displayText'],
-        "displayText" : "test",
+        "displayText" : x,
         # "data": data,
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
