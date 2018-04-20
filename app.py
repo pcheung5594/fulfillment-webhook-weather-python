54349ea9b06bff508429a188a8719684be3128c2
pcheung5594 <ppc5025@gmail.com>
2018-04-20 20:47:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -80,43 +80,11 @@ def makeYqlQuery(req):
 
 
 def makeWebhookResult(data):
-    #query = data.get('query')
-    #if query is None:
-    #    return {}
 
-    #result = query.get('results')
-    #if result is None:
-    #    return {}
-
-    #channel = result.get('channel')
-    #if channel is None:
-    #    return {}
-
-    #item = channel.get('item')
-    #location = channel.get('location')
-    #units = channel.get('units')
-    #if (location is None) or (item is None) or (units is None):
-    #    return {}
-
-    #condition = item.get('condition')
-    #if condition is None:
-    #    return {}
-
-    # print(json.dumps(item, indent=4))
-
-    #speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
-    #         ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-    #print("Response:")
-    #print(speech)
-    x = json.dumps(data)
+    #x = json.dumps(data)
     return {
-        #"speech": speech,
-        #"displayText": speech,
-        "speech": "test",
-        #"speech": data['displayText'],
-        #"displayText": data['displayText'],
-        "displayText" : x,
+        "speech": data['displayText'],
+        "displayText": data['displayText'],
         # "data": data,
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
